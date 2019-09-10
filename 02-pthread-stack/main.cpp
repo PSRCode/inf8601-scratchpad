4a423f4573013950eb86f0500820ea982d40ea21
Jonathan Rajotte <jonathan.r.julien@gmail.com>
2019-09-09 21:02:55 -0400

    Provide more context to the user
    Print the stack size in human readable format.
    Explain why the calculated size is not exactly the passed stack size.
    Print the default guard size.
    Be a bit more verbose on all printout to provide more context.
    
    While there simplify stuff a bit:
    
      Get rid of ulong coming from QT,
      Remove the typedef
        Remove some confusion that could occur with the standard "arg" coming
        from the command line.
      Remove unecessary argc & argv
    
    Signed-off-by: Jonathan Rajotte <jonathan.r.julien@gmail.com>

--- 02-pthread-stack/main.cpp
+++ 02-pthread-stack/main.cpp
@@ -1,44 +1,64 @@
 #include <QDebug>
 #include <pthread.h>
 
-typedef struct thread_arg {
-    ulong rsp;
-} arg_t;
+struct thread_arg {
+    unsigned long rsp;
+};
 
 void *thread(void *args)
 {
-    register ulong rsp asm("rsp");
-    arg_t *arg = (arg_t *) args;
+    register unsigned long rsp asm("rsp");
+    struct thread_arg *arg = (struct thread_arg *) args;
     arg->rsp = rsp;
     return 0;
 }
 
-int main(int argc, char *argv[])
+size_t get_defaul_stack_guard_size(void)
 {
-    (void) argc; (void) argv;
+    size_t value;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
 
+    (void) pthread_attr_getguardsize(&attr, &value);
+    /*
+     * No error checking here, given that you are on linux this should never
+     * fail. See "man pthread_attr_getguardsize".
+     */
+
+    pthread_attr_destroy(&attr);
+    return value;
+}
+
+int main(void)
+{
     int n = 4;
     pthread_t t[n];
-    arg_t args[n];
+    struct thread_arg args[n];
+    size_t stack_size = (1 << 15); /* 32KB */
+
+    qDebug() << "Creating threads with a stack size of " << stack_size << " bytes";
 
     for (int i = 0; i < n; i++) {
         pthread_attr_t attr;
-        size_t ss = (1 << 15);
+
         pthread_attr_init(&attr);
-        pthread_attr_setstacksize(&attr, ss);
+        pthread_attr_setstacksize(&attr, stack_size);
         pthread_create(&t[i], &attr, thread, &args[i]);
         pthread_attr_destroy(&attr);
     }
 
     for (int i = 0; i < n; i++) {
         pthread_join(t[i], NULL);
-        qDebug() << i << (void *)args[i].rsp;
+        qDebug() << "Address of the stack of thread #"<< i << ":" << (void *)args[i].rsp;
     }
 
     for (int i = 0; i < n - 1; i++) {
-        ulong space = args[i].rsp - args[i + 1].rsp;
-        qDebug() << "space" << space;
+        unsigned long space = args[i].rsp - args[i + 1].rsp;
+        qDebug() << "Space between previous stack" << space << "bytes";
     }
+
+    qDebug() << "The calculated stack size is bigger because of the stack guard.";
+    qDebug() << "Default value of the stack guard on your system is:" << get_defaul_stack_guard_size() ;
 
     return 0;
 }
